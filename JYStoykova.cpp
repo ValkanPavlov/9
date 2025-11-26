fc3163d77ae05da3398f38c559ef08e69eaf66ae
JYStoykova24 <JYStoykova24@codingburgas.bg>
2025-11-26 21:58:45 +0200

    Home assignment bubble sort

---
+++ JYStoykova.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 3};
+    int n = 5;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
